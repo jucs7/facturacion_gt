b2fbeab7cb557ed8de80943999b0bba40a6f1957
Juan <jucs0709@gmail.com>
2024-12-04 16:16:50 -0500

    feat: stock alerts

--- src/Handler/StockHandler.php
+++ src/Handler/StockHandler.php
@@ -51,11 +51,53 @@ class StockHandler {
 
       // Alerta de stock
       if ($producto->get('field_stock')->value === 0) {
+        $alerta = Node::create([
+          'type' => 'alerta_de_stock',
+          'title' => 'Alerta de stock ' . $producto->get('field_nombre')->value,
+          'field_producto' => $producto,
+          'field_tipo_de_alerta' => [
+            'value' => 'Unidades agotadas',
+            'format' => 'plain_text'
+          ],
+          'field_unidades_disponibles' => [
+            'value' => $producto->get('field_stock')->value,
+            'format' => 'number'
+          ],
+          'body' => [
+            'value' => 'Unidades disponibles del producto ' . $producto->get('field_nombre')->value . 
+              ': ' . $producto->get('field_stock')->value,
+            'format' => 'plain_text'
+          ],
+        ]);
+
+        $alerta->save();
+
         \Drupal::logger('facturacion_gt')->notice('ALERTA DE STOCK, @PRODUCTO AGOTADO', [
           '@PRODUCTO' => $producto->get('field_nombre')->value
         ]);
       }
       else if ($producto->get('field_stock')->value <= $producto->get('field_stock_de_emergencia')->value){
+        $alerta = Node::create([
+          'type' => 'alerta_de_stock',
+          'title' => 'Alerta de stock ' . $producto->get('field_nombre')->value,
+          'field_producto' => $producto,
+          'field_tipo_de_alerta' => [
+            'value' => 'Pocas unidades',
+            'format' => 'plain_text'
+          ],
+          'field_unidades_disponibles' => [
+            'value' => $producto->get('field_stock')->value,
+            'format' => 'number'
+          ],
+          'body' => [
+            'value' => 'Unidades disponibles del producto ' . $producto->get('field_nombre')->value . 
+              ': ' . $producto->get('field_stock')->value,
+            'format' => 'plain_text'
+          ],
+        ]);
+
+        $alerta->save();
+
         \Drupal::logger('facturacion_gt')->notice('ALERTA DE POCAS UNIDADES DE STOCK, @PRODUCTO UNIDADES: @EXISTENCIAS', [
           '@PRODUCTO' => $producto->get('field_nombre')->value,
           '@EXISTENCIAS' => $producto->get('field_stock')->value
