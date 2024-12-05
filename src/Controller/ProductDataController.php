d1d7201db6b428fc205db36d69c3c034999291ab
Juan <jucs0709@gmail.com>
2024-12-05 11:51:35 -0500

    fix(ProductDataController): fixed null values

--- src/Controller/ProductDataController.php
+++ src/Controller/ProductDataController.php
@@ -11,14 +11,14 @@ class ProductDataController extends ControllerBase{
     $product = Node::load($pid);
     if ($product) {
       return new JsonResponse([
-        'producto' => $product->getTitle(),
-        'field_categoria' => $product->get('field_categoria')->entity->getName(),
-        'field_codigo' => $product->get('field_codigo')->value,
-        'field_nombre' => $product->get('field_nombre')->value,
-        'field_precio' => $product->get('field_precio')->value,
-        'field_proveedor' => $product->get('field_proveedor')->entity->getDisplayName(),
-        'field_stock' => $product->get('field_stock')->value,
-        'field_stock_de_emergencia' => $product->get('field_stock_de_emergencia')->value,
+        'producto' => $product->getTitle() ?? '',
+        'field_categoria' => $product->get('field_categoria')->entity->getName() ?? '',
+        'field_codigo' => $product->get('field_codigo')->value ?? '',
+        'field_nombre' => $product->get('field_nombre')->value ?? '',
+        'field_precio' => $product->get('field_precio')->value ?? '',
+        'field_proveedor' => $product->get('field_proveedor')->entity->getDisplayName() ?? '',
+        'field_stock' => $product->get('field_stock')->value ?? '',
+        'field_stock_de_emergencia' => $product->get('field_stock_de_emergencia')->value ?? '',
       ]);
     }
 
